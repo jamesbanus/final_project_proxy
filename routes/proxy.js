0b548de838de3ad1de022ea535ad7f23cbe500e5
James Banus <jamesbanus@jamess-mbp.home>
2023-12-19 17:00:22 +0000

    added more proxies

--- routes/proxy.js
+++ routes/proxy.js
@@ -3,22 +3,23 @@ const router = express.Router();
 const axios = require("axios");
 const { authorization } = require("../utils/apiUrls");
 
-const cache = {};
+let cache = {};
 
 setInterval(() => {
   cache = {};
 }, 86400000);
 
 router.get("/", async (req, res) => {
-  console.log(req.query.url);
+  //   console.log(req.query.url);
   const url = Buffer.from(req.query.url, "base64");
+  //   console.log(url.toString());
   if (!url.toString().startsWith("https://api.themoviedb.org/3")) {
     return;
   }
 
   if (cache[url]) {
     res.send(cache[url]);
-    console.log("Found inCache");
+    // console.log("found in cache");
     return;
   }
 
@@ -32,5 +33,173 @@ router.get("/", async (req, res) => {
     res.send(result);
   }
 });
+
+router.get("/database", async (req, res) => {
+  // console.log(req.query.url);
+  const url = Buffer.from(req.query.url, "base64");
+  //   console.log(url.toString());
+  if (!url.toString().startsWith("http://localhost:4000/account")) {
+    return;
+  }
+
+  try {
+    const result = await axios.get(url);
+    res.send(result.data);
+  } catch (result) {
+    res.send(result);
+  }
+});
+
+router.post("/databasePost/:email/:password", async (req, res) => {
+  // console.log(req.query.url);
+  const url = Buffer.from(req.query.url, "base64");
+  console.log(url.toString());
+  if (
+    !url
+      .toString()
+      .startsWith(
+        ("http://localhost:4000/account/register",
+        "http://localhost:4000/account/login")
+      )
+  ) {
+    return;
+  }
+
+  const email = req.params.email;
+  const password = req.params.password;
+
+  const accountInfo = { email: email, password: password };
+
+  try {
+    const result = await axios.post(url, accountInfo);
+    res.send(result.data);
+  } catch (result) {
+    res.send(result);
+  }
+});
+
+router.get("/database/:token", async (req, res) => {
+  // console.log(req.query.url);
+  const url = Buffer.from(req.query.url, "base64");
+  console.log(url.toString());
+  if (!url.toString().startsWith("http://localhost:4000/useractions")) {
+    return;
+  }
+
+  const token = req.params.token;
+
+  try {
+    const result = await axios.get(url, {
+      headers: { token: token },
+    });
+    res.send(result.data);
+    // console.log(result.data);
+    //   cache[url] = result.data;
+  } catch (result) {
+    res.send(result);
+  }
+});
+
+router.post("/databasePostFave/:token/:movieid/:fave", async (req, res) => {
+  // console.log(req.query.url);
+  const url = Buffer.from(req.query.url, "base64");
+  console.log(url.toString());
+  if (!url.toString().startsWith("http://localhost:4000/useractions")) {
+    return;
+  }
+
+  const token = req.params.token;
+  const movieid = req.params.movieid;
+  const fave = req.params.fave;
+
+  const faveInfo = { movie_id: movieid, favourite: fave };
+
+  try {
+    const result = await axios.post(url, faveInfo, {
+      headers: { token: token },
+    });
+    res.send(result.data);
+  } catch (result) {
+    res.send(result);
+  }
+});
+
+router.patch("/databasePatchFave/:token/:movieid/:fave", async (req, res) => {
+  // console.log(req.query.url);
+  const url = Buffer.from(req.query.url, "base64");
+  console.log(url.toString());
+  if (!url.toString().startsWith("http://localhost:4000/useractions")) {
+    return;
+  }
+
+  const token = req.params.token;
+  const movieid = req.params.movieid;
+  const fave = req.params.fave;
+
+  const faveInfo = { movie_id: movieid, favourite: fave };
+
+  try {
+    const result = await axios.patch(url, faveInfo, {
+      headers: { token: token },
+    });
+    res.send(result.data);
+  } catch (result) {
+    res.send(result);
+  }
+});
+
+router.post(
+  "/databasePostRating/:token/:movieid/:currentRating",
+  async (req, res) => {
+    // console.log(req.query.url);
+    const url = Buffer.from(req.query.url, "base64");
+    console.log(url.toString());
+    if (!url.toString().startsWith("http://localhost:4000/useractions")) {
+      return;
+    }
+
+    const token = req.params.token;
+    const movieid = req.params.movieid;
+    const currentRating = req.params.currentRating;
+
+    const ratingInfo = { movie_id: movieid, rating: currentRating };
+
+    try {
+      const result = await axios.post(url, ratingInfo, {
+        headers: { token: token },
+      });
+      res.send(result.data);
+    } catch (result) {
+      res.send(result);
+    }
+  }
+);
+
+router.patch(
+  "/databasePatchRating/:token/:movieid/:currentRating",
+  async (req, res) => {
+    // console.log(req.query.url);
+    const url = Buffer.from(req.query.url, "base64");
+    console.log(url.toString());
+    if (!url.toString().startsWith("http://localhost:4000/useractions")) {
+      return;
+    }
+
+    const token = req.params.token;
+    const movieid = req.params.movieid;
+    const currentRating = req.params.currentRating;
+
+    const ratingInfo = { movie_id: movieid, rating: currentRating };
+
+    try {
+      const result = await axios.patch(url, ratingInfo, {
+        headers: { token: token },
+      });
+      res.send(result.data);
+    } catch (result) {
+      res.send(result);
+    }
+  }
+);
 
 module.exports = router;
